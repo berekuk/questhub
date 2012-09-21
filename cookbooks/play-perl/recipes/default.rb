90afd6e1741196c8af0d70059314f041aa439a00
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2012-09-22 00:15:18 +0400

    resolv.conf

--- cookbooks/play-perl/recipes/default.rb
+++ cookbooks/play-perl/recipes/default.rb
@@ -5,3 +5,10 @@ package 'git'
 package 'libdancer-perl'
 
 include_recipe "mongodb::default"
+
+template "/etc/resolv.conf" do
+  source "resolv.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+end
