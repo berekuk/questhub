0c6f8783495c677de8eb90ce599f8b9a12dbdab0
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2012-09-22 00:58:06 +0400

    nginx as a http proxy

--- cookbooks/play-perl/recipes/default.rb
+++ cookbooks/play-perl/recipes/default.rb
@@ -3,7 +3,6 @@ require_recipe "mongodb"
 package 'vim'
 package 'git'
 package 'libdancer-perl'
-package 'nginx'
 
 include_recipe "mongodb::default"
 
@@ -19,4 +18,26 @@ template "/etc/resolv.conf" do
   owner "root"
   group "root"
   mode 0644
+end
+
+# nginx
+directory '/web' # logs
+
+package 'nginx'
+
+file '/etc/nginx/sites-enabled/default' do
+    action :delete
+end
+
+template "/etc/nginx/sites-enabled/play-perl.org" do
+  source "nginx-site.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+end
+
+bash "restart nginx" do
+  code <<-EOH
+/etc/init.d/nginx restart
+EOH
 end
