05aeab7c1c0c763e54af87494116e931badb48f6
Andrei Mishchenko <me@druxa.ru>
2012-09-22 12:13:25 +0000

    fix infinite redirect while authorizing; /auth/user api handle

--- app/lib/Play/Auth.pm
+++ app/lib/Play/Auth.pm
@@ -8,9 +8,17 @@ use Play::Quests;
 auth_twitter_init();
 
 before sub {
+    return if request->path =~ m{/auth/twitter/callback};
     if (not session('twitter_user')) {
         redirect auth_twitter_authenticate_url;
     }
 };
+
+prefix '/auth';
+
+get '/user' => sub {
+    return { twitter => session('twitter_user') };
+};
+
 
 true;
