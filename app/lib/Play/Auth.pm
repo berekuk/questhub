b26a556d9b5f6820755f7b00d08cacd812529e9a
Andrei Mishchenko <me@druxa.ru>
2012-09-22 14:08:09 +0000

    works!

--- app/lib/Play/Auth.pm
+++ app/lib/Play/Auth.pm
@@ -3,9 +3,14 @@ package Play::Auth;
 use Dancer ':syntax';
 use Dancer::Plugin::Auth::Twitter;
 
+use MongoDB;
+
 use Play::Quests;
 
+use Data::Dumper;
 auth_twitter_init();
+
+my $users = MongoDB::Connection->new(host => 'localhost', port => 27017)->play_perl->users;
 
 prefix '/auth';
 
@@ -13,13 +18,57 @@ get '/twitter' => sub {
     if (not session('twitter_user')) {
         redirect auth_twitter_authenticate_url;
     } else {
+        
+        my $twitter_login = session->{twitter_user}->{screen_name};
+        my $user = $users->find_one({twitter=>{login=>$twitter_login}});
+        debug "found user!! ". Dumper($user);
+        if ($user) {
+            session 'login' => $user->{login};
+        }
     	redirect "/";
     }
 };
 
+prefix '/api';
+
+
 get '/user' => sub {
-    return { twitter => session('twitter_user') };
+    if (request->params->{login}) {
+         return { error => "TODO" };
+    }
+    if (request->params->{twitter_login}) {
+         return { error => "TODO" };
+    }
+    
+    my $login = session->{login};
+    #TODO: cache in session
+    my $user = $users->find_one({login => $login});
+    return { 
+        twitter => { login => $user->{twitter}->{login} },
+        login => $user->{login},   
+    };
 };
 
+get '/session' => sub {
+    return {%{session()}};
+};
+
+get '/new_login' => sub {
+    if (not session('twitter_user')) {
+        return { error => "not authorized" };
+    }
+    my $twitter_login = session('twitter_user')->{screen_name};
+    my $login = request->params->{login};
+    if ($users->find_one({login => $login})) {
+        return { error => "Already exists" };
+    }
+    if ($users->find_one({twitter=>{login => $twitter_login}})) {
+        return { error => "Already bound" };
+    }
+    my $user = {login => $login, twitter => {login => $twitter_login}};
+    session 'login' => $login;
+    $users->insert($user);
+    return { status => "ok", user => $user };
+};
 
 true;
