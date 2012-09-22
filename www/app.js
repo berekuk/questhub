2e4fc6c23773dd26db70c08b0a78aeebf033ddfd
Kirill Dmitrenko <kdmitrenko@gmail.com>
2012-09-22 17:53:24 +0400

    www: current user view

--- www/app.js
+++ www/app.js
@@ -1,5 +1,5 @@
 $(function () {
-    var appView = new pp.views.App({el: $('#layout')});
+    var appView = pp.app.view = new pp.views.App({el: $('#layout')});
 
     var router = new (Backbone.Router.extend({
         routes: {
@@ -11,12 +11,12 @@ $(function () {
             var homeView = new pp.views.Home();
             appView.setPageView(homeView);
         },
+
         questAdd: function() {
             var questAddView = new pp.views.QuestAdd();
             appView.setPageView(questAddView);
         },
     }))();
-//
-    appView.render();
+
     Backbone.history.start();
 });
