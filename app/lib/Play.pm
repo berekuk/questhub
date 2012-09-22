324e860edcbc33af023ae223bdad13f3a0369d06
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2012-09-22 16:48:14 +0000

    refactor routing code into Play::Route::* modules
    and other changes, don't remember exactly :(

--- app/lib/Play.pm
+++ app/lib/Play.pm
@@ -1,70 +1,7 @@
 package Play;
 use Dancer ':syntax';
 
-our $VERSION = '0.1';
-
-set serializer => 'JSON';
-
-prefix '/api';
-
-use Play::Quests;
-use Play::Auth;
-
-my $quests = Play::Quests->new;
-
-post '/quest/:id' => sub {
-    die "not logged in" unless session->{login};
-    my $updated_id = $quests->update(
-        param('id'),
-        {
-            user => session->{login},
-            map { param($_) ? ($_ => param($_)) : () } qw/ name status /,
-        }
-    );
-    return {
-        status => 'ok',
-        id => $updated_id,
-    }
-};
-
-post '/quest' => sub {
-    die "not logged in" unless session->{login};
-    my $id = $quests->add({
-        user => session->{login},
-        name => param('name'),
-        status => 'open',
-    });
-    return {
-        status => 'ok',
-        id => $id,
-    }
-};
-
-get '/quests' => sub {
-    return $quests->list({
-        map { param($_) ? ($_ => param($_)) : () } qw/ user status /,
-    });
-};
-
-get '/quest/:id' => sub {
-    return $quests->get(param('id'));
-};
-
-get '/get_login' => sub {
-    return {
-        status => 'ok',
-        logged => (defined session->{login} ? 1 : 0),
-        login => session->{login},
-    };
-};
-
-get '/logout' => sub {
-
-    session->destroy(session); #FIXME: workaround a buggy Dancer::Session::MongoDB
-
-    return {
-        status => 'ok'
-    };
-};
+use Play::Route::Users;
+use Play::Route::Quests;
 
 true;
---
+++ app/lib/Play/Route/Quests.pm
@@ -0,0 +1,47 @@
+package Play::Route::Quests;
+
+use Dancer ':syntax';
+prefix '/api';
+
+use Play::Quests;
+my $quests = Play::Quests->new;
+
+post '/quest/:id' => sub {
+    die "not logged in" unless session->{login};
+    my $updated_id = $quests->update(
+        param('id'),
+        {
+            user => session->{login},
+            map { param($_) ? ($_ => param($_)) : () } qw/ name status /,
+        }
+    );
+    return {
+        result => 'ok',
+        id => $updated_id,
+    }
+};
+
+post '/quest' => sub {
+    die "not logged in" unless session->{login};
+    my $id = $quests->add({
+        user => session->{login},
+        name => param('name'),
+        status => 'open',
+    });
+    return {
+        ok => 1,
+        id => $id,
+    }
+};
+
+get '/quests' => sub {
+    return $quests->list({
+        map { param($_) ? ($_ => param($_)) : () } qw/ user status /,
+    });
+};
+
+get '/quest/:id' => sub {
+    return $quests->get(param('id'));
+};
+
+true;
