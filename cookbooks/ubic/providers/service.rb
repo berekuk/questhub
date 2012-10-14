04d8b4758c9487161909f8a9e90144cdcc215ab9
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2012-10-15 02:02:55 +0400

    ubic template notifies fixme comment; bring back ubic status checks

--- cookbooks/ubic/providers/service.rb
+++ cookbooks/ubic/providers/service.rb
@@ -1,13 +1,13 @@
 action :start do
   execute "start ubic service #{new_resource.name}" do
-#    not_if "ubic status #{new_resource.name}"
+    not_if "ubic status #{new_resource.name}"
     command "ubic start #{new_resource.name}"
   end
 end
 
 action :stop do
   execute "stop ubic service #{new_resource.name}" do
-#    only_if "ubic status #{new_resource.name}"
+    only_if "ubic status #{new_resource.name}"
     command "ubic stop #{new_resource.name}"
   end
 end
@@ -24,6 +24,6 @@ action :install do
     owner "root"
     group "root"
     mode "0644"
-    notifies :restart, "ubic_service[#{new_resource.name}]"
+    notifies :restart, "ubic_service[#{new_resource.name}]" # FIXME - doesn't work for some reason, see https://gist.github.com/3889852
   end
 end
