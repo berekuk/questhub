ae0089def0851209cec3b65de6b8a27012cb52f5
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2012-10-30 23:10:14 +0000

    render fatal (non-json) errors correctly

--- www/views/error.js
+++ www/views/error.js
@@ -3,7 +3,13 @@ pp.views.Error = Backbone.View.extend({
     template: _.template($('#template-error').text()),
 
     render: function () {
-        this.$el.html(this.template({response: jQuery.parseJSON(this.options.response.responseText) }));
+        var response = {};
+        try {
+            response = jQuery.parseJSON(this.options.response.responseText);
+        }
+        catch(e) {}
+
+        this.$el.html(this.template({ response: response }));
         return this;
     }
 });
