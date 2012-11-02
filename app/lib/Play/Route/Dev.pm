84d593407ac39dc41caff6770dd0d68d3edcb909
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2012-11-02 22:31:01 +0000

    update backend API docs

--- app/lib/Play/Route/Dev.pm
+++ app/lib/Play/Route/Dev.pm
@@ -3,9 +3,11 @@ package Play::Route::Dev;
 use Dancer ':syntax';
 prefix '/api/dev';
 
-get '/session/:name' => sub {
-    my $name = param('name');
-    return session($name);
-};
+if ($ENV{DEV_MODE}) {
+    get '/session/:name' => sub {
+        my $name = param('name');
+        return session($name);
+    };
+}
 
 true;
