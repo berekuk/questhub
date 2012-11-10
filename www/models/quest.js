672f2eca25aa03af3ed3ae4c67e7c74948e7a6b4
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2012-11-10 20:13:01 +0000

    refetch user on quest close/reopen

--- www/models/quest.js
+++ www/models/quest.js
@@ -1,15 +1,25 @@
 pp.models.Quest = Backbone.Model.extend({
     urlRoot: '/api/quest',
+
     close: function() {
-        this.save(
-            { "status": "closed" },
-            { error: pp.app.onError }
-        );
+        this._setStatus('closed');
     },
+
     reopen: function() {
+        this._setStatus('open');
+    },
+
+    _setStatus: function(st) {
         this.save(
-            { "status": "open" },
-            { error: pp.app.onError }
+            { "status": st },
+            {
+                success: function () {
+                    // quest status update causes update in points
+                    pp.app.user.fetch();
+                },
+                error: pp.app.onError
+            }
         );
     }
+
 });
