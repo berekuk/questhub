b8e25e018d5d024c870389e0c43fdefd5bb7165f
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2012-11-25 16:39:25 +0000

    prevent accidental double-posting of comment

--- www/views/comment/collection.js
+++ www/views/comment/collection.js
@@ -1,7 +1,8 @@
 pp.views.CommentCollection = Backbone.View.extend({
 
     events: {
-        'click .submit': 'postComment'
+        'click .submit': 'postComment',
+        'keyup [name=comment]': 'validate'
     },
 
     template: _.template($('#template-comment-collection').text()),
@@ -18,7 +19,19 @@ pp.views.CommentCollection = Backbone.View.extend({
         return this;
     },
 
+    validate: function (e) {
+        if ($(e.target).val()) {
+            this.$('.submit').removeClass('disabled');
+        }
+        else {
+            this.$('.submit').addClass('disabled');
+        }
+    },
+
     renderOne: function (comment) {
+        this.$('[name=comment]').attr({ disabled: null });
+        this.$('[name=comment]').val('');
+
         var view = new pp.views.Comment({ model: comment });
         var cl = this.$el.find('.comments-list');
         cl.show();
@@ -26,14 +39,25 @@ pp.views.CommentCollection = Backbone.View.extend({
     },
 
     postComment: function() {
+        if (this.$('.submit').hasClass('disabled')) {
+            return;
+        }
+        this.$('[name=comment]').attr({ disabled: 'disabled' });
+
         this.options.comments.create({
             'author': pp.app.user.get('login'),
             'body': this.$('[name=comment]').val()
         },
         {
             'success': this.renderOne,
-            'error': pp.app.onError
+            'error': this.onError
         });
+    },
+
+    onError: function(model, response) {
+        pp.app.onError(model, response);
+        this.$('[name=comment]').attr({ disabled: null });
+        // note that we don't clear textarea's val() to keep the comment from vanishing
     },
 
     onReset: function () {
