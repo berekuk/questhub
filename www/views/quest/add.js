3ab76419ca4c97a240a6f9a696ef9a03ddcee418
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2012-11-25 16:18:57 +0000

    quest type - choose and display

--- www/views/quest/add.js
+++ www/views/quest/add.js
@@ -1,6 +1,8 @@
 pp.views.QuestAdd = Backbone.View.extend({
     events: {
-        'click .quest-add': 'saveToModel'
+        'click .quest-add': 'submit',
+        'click .quest-type-select button': 'setType',
+        'keyup [name=name]': 'validate'
     },
 
     template: _.template($('#template-quest-add').text()),
@@ -8,12 +10,49 @@ pp.views.QuestAdd = Backbone.View.extend({
     initialize: function() {
         _.bindAll(this);
         this.render();
+        this.submitted = false;
+        this.gotType = false;
+        this.validate();
+    },
+
+    setType: function(e) {
+        // Radio buttons - activate clicked button and disactivate all the others.
+        // We can't use native radio buttons from bootstrap because of unpredictable event triggering order, btw.
+        // See http://stackoverflow.com/questions/9262827/twitter-bootstrap-onclick-event-on-buttons-radio for details.
+        $(e.target.parentElement).find('.active').removeClass('active');
+        $(e.target).button('toggle');
+        this.gotType = true;
+        this.validate();
+    },
+
+    disable: function() {
+        this.$('.quest-add').addClass('disabled');
+        this.enabled = false;
+    },
+
+    enable: function() {
+        this.$('.quest-add').removeClass('disabled');
+        this.enabled = true;
+        this.submitted = false;
+    },
+
+    validate: function() {
+        if (this.submitted || !this.gotType || !this.getDescription()) {
+            this.disable();
+        }
+        else {
+            this.enable();
+        }
+    },
+
+    getDescription: function() {
+        return this.$('[name=name]').val();
     },
 
     render: function () {
         this.setElement($(this.template()));
 
-        this.$el.find('#addQuest').modal().css({
+        this.$('#addQuest').modal().css({
             'width': function () {
                 return ($(document).width() * .8) + 'px';
             },
@@ -23,23 +62,31 @@ pp.views.QuestAdd = Backbone.View.extend({
         });
     },
 
-    saveToModel: function() {
-        console.log(this.collection);
+    submit: function() {
+        if (!this.enabled) {
+            return;
+        }
+
+        var type = this.$('.quest-type-select button.active').attr('quest-type');
+        if (!type) {
+            type = 'other';
+        }
+
         var model = new this.collection.model();
         model.save({
-            name: this.$('[name=name]').val()
+            name: this.getDescription(),
+            type: type
         }, {
             'success': this.onSuccess,
             'error': pp.app.onError
         });
 
-        // FIXME - this still doesn't disable click event!
-        this.$el.find('.quest-add').addClass('disabled');
+        this.submitted = true;
+        this.validate();
     },
 
     onSuccess: function (model) {
-        console.log(model);
         this.collection.add(model);
-        this.$el.find('#addQuest').modal('hide');
-    }
+        this.$('#addQuest').modal('hide');
+    },
 });
