ab17112b29b43f1c8abb240d52f712d2c5689959
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2012-12-04 22:42:19 +0400

    extra apt-get update in ubic cookbook

--- cookbooks/ubic/recipes/default.rb
+++ cookbooks/ubic/recipes/default.rb
@@ -1,10 +1,13 @@
 ppa "berekuk/ubic"
 
-# FIXME - we must do 'apt-get update' first!
-# provisioning will fail and you'll have to fix it manually
+execute "ubic apt-get update" do
+    command "apt-get update"
+    not_if "apt-cache policy ubic | grep Candidate"
+end
+
 package "ubic"
 
-execute "setup ubic" do
-    command "ubic-admin setup --batch-mode"
-    not_if "test -f /etc/ubic.cfg"
-end
+#execute "setup ubic" do
+#    command "ubic-admin setup --batch-mode"
+#    not_if "test -f /etc/ubic.cfg"
+#end
