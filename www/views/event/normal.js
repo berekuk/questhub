1d4a8fcee4fb98226d1b5a91f1379c59c534a3b1
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-02-08 22:31:42 +0000

    render add-quest events

--- www/views/event/normal.js
+++ www/views/event/normal.js
@@ -1,8 +1,11 @@
 pp.views.Event = Backbone.View.extend({
-    template: _.template($('#template-event').text()),
+    template: function () {
+        return _.template($('#template-event-' + this.model.get('action') + '-' + this.model.get('object_type')).text());
+    },
 
     initialize: function () {
         console.log('event ' + this.model.id);
-        this.setElement($(this.template(this.model.toJSON())));
+        var template = this.template();
+        this.setElement($(template(this.model.toJSON())));
     }
 });
