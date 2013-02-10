50c664af1841017bd4d773d74e7159160916c093
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-02-10 20:42:27 +0400

    edit_tools is a partial

--- www/views/proto.js
+++ www/views/proto.js
@@ -1,6 +1,7 @@
 pp.View.Base = Backbone.View.extend({
     partial: {
-        user: _.template($('#partial-user').text())
+        user: _.template($('#partial-user').text()),
+        edit_tools: _.template($('#partial-edit-tools').text())
     }
 });
 
