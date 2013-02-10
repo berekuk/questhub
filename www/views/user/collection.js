c1498fa7d930a48a15b1dfe1ab00e622070c9408
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-02-10 06:13:41 +0000

    hide players without quests by default

--- www/views/user/collection.js
+++ www/views/user/collection.js
@@ -2,12 +2,33 @@ pp.views.UserCollection = Backbone.View.extend({
 
     template: _.template($('#template-user-collection').text()),
 
-    initialize: function () {
-        this.options.users.on('reset', this.render, this);
+    events: {
+        "click .show-all": "showAll",
     },
 
-    render: function (collection) {
-        this.$el.html(this.template({ users: this.options.users }));
+    initialize: function () {
+        this.options.users.on('reset', this.render, this);
+        this.all = false;
+    },
+
+    showAll: function () {
+        this.all = true;
+        this.render();
+    },
+
+    render: function () {
+        var users = this.options.users;
+        console.log('render ' + this.all);
+
+        var that = this;
+        users = users.filter(function(user) {
+            if (that.all || user.get('open_quests') > 0) {
+                return true;
+            }
+            return false;
+        });
+
+        this.$el.html(this.template({ users: users, all: this.all }));
         return this;
     }
 });
