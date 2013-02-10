f59ba8269b14e143b90e6ff8d236d83ce8d48435
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-02-10 08:28:31 +0000

    quest-small is pp.View.Common

--- www/views/proto.js
+++ www/views/proto.js
@@ -11,12 +11,12 @@ pp.View.Base = Backbone.View.extend({
 //   selfRender: this flag causes initialize() to call render()
 //   serialize: should prepare params for the template
 //   features: array with features that should be enabled in html after rendering; possible values: ['timeago', 'tooltip']
+//   afterRender: optional render() method modifier
 pp.View.Common = pp.View.Base.extend({
 
     // TODO - detect 't' default value from the class name somehow? is it possible in JS?
 
     initialize: function () {
-        console.log('initialize ' + this.t);
         this.template = _.template($('#template-' + this.t).text());
         if (this.selfRender) {
             this.render();
@@ -30,7 +30,6 @@ pp.View.Common = pp.View.Base.extend({
     features: [],
 
     render: function () {
-        console.log('initialize ' + this.t);
         var params = this.serialize();
         params.partial = this.partial;
         this.$el.html(this.template(params));
@@ -43,12 +42,18 @@ pp.View.Common = pp.View.Base.extend({
                 that.$el.find('time.timeago').timeago();
             }
             else if (feature == 'tooltip') {
-                this.$el.find('[data-toggle=tooltip]').tooltip();
+                that.$el.find('[data-toggle=tooltip]').tooltip();
             } else {
                 console.log("unknown feature: " + feature);
             }
         });
 
+        this.afterRender();
+
         return this;
+    },
+
+    afterRender: function () {
     }
+
 });
