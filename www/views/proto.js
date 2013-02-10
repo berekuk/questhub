a892984f837d0943d2a69dcde91f2951f8917fde
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-02-10 09:13:28 +0000

    subviews support in pp.View.Common

--- www/views/proto.js
+++ www/views/proto.js
@@ -4,14 +4,25 @@ pp.View.Base = Backbone.View.extend({
     }
 });
 
-// common play-perl view
-// it declares render() itself
-// options:
-//   t: 'blah' - use '#template-blah' template
-//   selfRender: this flag causes initialize() to call render()
-//   serialize: should prepare params for the template
-//   features: array with features that should be enabled in html after rendering; possible values: ['timeago', 'tooltip']
-//   afterRender: optional render() method modifier
+/* common play-perl view
+ * it declares render() itself
+ * options:
+ *   t: 'blah' - use '#template-blah' template
+ *   selfRender: this flag causes initialize() to call render()
+ *   serialize: should prepare params for the template
+ *   features: array with features that should be enabled in html after rendering; possible values: ['timeago', 'tooltip']
+ *   afterRender: optional render() method modifier
+ *   subviews: events-style hash with subviews; see assign pattern in http://ianstormtaylor.com/assigning-backbone-subviews-made-even-cleaner/
+ *
+ * subviews usage example:
+ *   subviews: {
+ *     '.foo-item': function() { return new pp.views.Foo() },
+ *     '.bar-item': 'barSubview',
+ *   },
+ *   barSubview: function() {
+ *      return new pp.views.Bar();
+ *   }
+*/
 pp.View.Common = pp.View.Base.extend({
 
     // TODO - detect 't' default value from the class name somehow? is it possible in JS?
@@ -28,6 +39,7 @@ pp.View.Common = pp.View.Base.extend({
     },
 
     features: [],
+    subviews: {},
 
     render: function () {
         var params = this.serialize();
@@ -49,6 +61,20 @@ pp.View.Common = pp.View.Base.extend({
         });
 
         this.afterRender();
+
+        // note that we're initializing subviews in render() for now
+        // that's not the best solution, see http://ianstormtaylor.com/rendering-views-in-backbonejs-isnt-always-simple/ for details
+        _.each(_.keys(this.subviews), function(key) {
+            var value = that.subviews[key];
+
+            var method = value;
+            if (!_.isFunction(method)) method = that[value];
+            if (!method) throw new Error('Method "' + value + '" does not exist');
+            method = _.bind(method, that);
+            var subview = method();
+
+            subview.setElement(that.$el.find(key)).render();
+        });
 
         return this;
     },
