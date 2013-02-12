9b65002fff691038d44532e09bfd7a94742b248d
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-02-12 21:28:07 +0000

    remove views/notify, nobody uses it

--- www/views/notify.js
+++
@@ -1,9 +0,0 @@
-pp.views.Notify = pp.View.Base.extend({
-
-    template: _.template($('#template-notify').text()),
-
-    render: function () {
-        this.$el.html(this.template({text: this.options.text }));
-        return this;
-    }
-});
