4ea95c4692aad6101ab16be6eef1a277e96e62ac
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-02-13 18:41:49 +0000

    reload the page on logout

--- www/views/user/current.js
+++ www/views/user/current.js
@@ -26,11 +26,10 @@ pp.views.CurrentUser = pp.View.Common.extend({
         this.model.fetch();
     },
 
-    logout: function () {
-        // FIXME - it's probably better to reload the whole page
+    logout: function (e) {
+        // TODO - fade to black until response
         $.post('/api/logout').always(function () {
-            this.model.fetch();
-            pp.app.router.navigate("/welcome", { trigger: true });
+            window.location = '/';
         }.bind(this));
     }
 });
