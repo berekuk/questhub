8f3884eb905c2e1207b76ad6d850244dcaf08632
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-02-14 07:18:29 +0400

    closed quests in /explore tab (two sub-tabs)

--- www/views/explore.js
+++ www/views/explore.js
@@ -5,24 +5,27 @@ pp.views.Explore = pp.View.Common.extend({
     selfRender: true,
 
     subviews: {
-        '.quests': function () {
-            var collection = new pp.models.QuestCollection([], {
-               'status': 'open'
-            });
-            collection.comparator = function(m1, m2) {
-                if (m1.like_count() > m2.like_count()) return -1; // before
-                if (m2.like_count() > m1.like_count()) return 1; // after
-
-                if (m1.comment_count() > m2.comment_count()) return -1;
-                if (m2.comment_count() > m1.comment_count()) return 1;
-                return 0; // equal
-            };
-            collection.fetch();
-            collection.sort();
-
-            return new pp.views.QuestCollection({
-                collection: collection
-            });
-        },
+        '#open-quests-tab': function () { return this.questSubview('open') },
+        '#closed-quests-tab': function () { return this.questSubview('closed') }
     },
+
+    questSubview: function (st) {
+        var collection = new pp.models.QuestCollection([], {
+           'status': st
+        });
+        collection.comparator = function(m1, m2) {
+            if (m1.like_count() > m2.like_count()) return -1; // before
+            if (m2.like_count() > m1.like_count()) return 1; // after
+
+            if (m1.comment_count() > m2.comment_count()) return -1;
+            if (m2.comment_count() > m1.comment_count()) return 1;
+            return 0; // equal
+        };
+        collection.fetch();
+        collection.sort();
+
+        return new pp.views.QuestCollection({
+            collection: collection
+        });
+    }
 });
