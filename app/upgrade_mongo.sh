81765989e9d521cd78288ed7539bbdb7bae35cbe
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-02-15 12:14:00 +0000

    embed mongo_setup code into upgrade_mongo.sh using heredoc

---
+++ app/upgrade_mongo.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+CODE=$(cat <<END
+use play
+db.events.find().snapshot().forEach(function (doc) {
+    if (doc.object_type == 'quest') {
+        doc.author = doc.object.user;
+    }
+    else if (doc.object_type == 'comment') {
+        doc.author = doc.object.author;
+    }
+    else if (doc.object_type == 'user') {
+        doc.author = doc.object.login;
+    }
+    db.events.save(doc);
+})
+END
+)
+
+echo "$CODE" | mongo
