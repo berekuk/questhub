4a406ec9ce4cb76f379fd10dc053ac3e20ca3c95
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-02-16 16:43:16 +0000

    confirm_email and resend_email_confirmation links on play-perl.org, not localhost!!

--- www/views/confirm-email.js
+++ www/views/confirm-email.js
@@ -2,7 +2,7 @@ pp.views.ConfirmEmail = pp.View.Common.extend({
     t: 'confirm-email',
     selfRender: true,
     afterInitialize: function () {
-        $.post('http://localhost:3000/api/register/confirm_email', this.options)
+        $.post('http://play-perl.org/api/register/confirm_email', this.options)
         .done(function () {
             $('.alert').alert('close');
             pp.app.view.notify('success', 'Email confirmed.');
