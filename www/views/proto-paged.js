92f0856ef42da8803f4d9e39e5a53c1652920c7f
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-02-18 00:11:39 +0000

    refactor quest/collection and user/collection common code into proto-paged

---
+++ www/views/proto-paged.js
@@ -0,0 +1,63 @@
+// collection which shows first N items, and then others can be fetched by clicking "show more" button
+// your view is going to need:
+// 1) template with a clickable '.show-more' element and with '.progress-spin' element
+// 2) listSelector, just like in AnyCollection
+// 3) generateItem, just like in AnyCollection
+//
+// This collection depends on pp.views.Progress, which depends on pp.View.Common... so it lives in a separate from proto.js file.
+pp.View.PagedCollection = pp.View.AnyCollection.extend({
+    events: {
+        "click .show-more": "showMore",
+    },
+
+    subviews: {
+        '.progress-spin': function () {
+            return new pp.views.Progress();
+        },
+    },
+
+    insertOne: function (el) {
+        this.$('.show-more').before(el);
+    },
+
+    activated: true,
+
+    pageSize: 100,
+
+    noProgress: function () {
+        this.$('.show-more').toggle(this.collection.gotMore);
+        this.$('.show-more').removeClass('disabled');
+        this.subview('.progress-spin').off();
+    },
+
+    afterInitialize: function () {
+        pp.View.AnyCollection.prototype.afterInitialize.apply(this, arguments);
+        this.subview('.progress-spin').on(); // app.js fetches the collection for the first time immediately
+
+        this.collection.once('reset', this.noProgress, this);
+        this.listenTo(this.collection, 'error', this.noProgress);
+        this.render();
+    },
+
+    showMore: function () {
+        console.log('showMore');
+        var that = this;
+
+        this.$('.show-more').addClass('disabled');
+        this.subview('.progress-spin').on();
+
+        this.collection.fetchMore(this.pageSize, {
+            error: function (collection, response) {
+                pp.app.onError(undefined, response);
+            }
+        }).always(function () {
+            that.noProgress();
+        });
+    },
+
+    afterRender: function () {
+        pp.View.AnyCollection.prototype.afterRender.apply(this, arguments);
+        this.$el.find('[data-toggle=tooltip]').tooltip('show');
+    }
+
+});
--- www/views/user/collection.js
+++ www/views/user/collection.js
@@ -1,61 +1,9 @@
-pp.views.UserCollection = pp.View.AnyCollection.extend({
+pp.views.UserCollection = pp.View.PagedCollection.extend({
     t: 'user-collection',
-
-    events: {
-        "click .show-more": "showMore",
-    },
-
-    subviews: {
-        '.progress-spin': function () {
-            return new pp.views.Progress();
-        },
-    },
-
     listSelector: '.users-list',
-    insertOne: function (el) {
-        this.$('.show-more').before(el);
-    },
-
-    activated: true,
-
-    noProgress: function () {
-        this.$('.show-more').toggle(this.collection.gotMore);
-        this.$('.show-more').removeClass('disabled');
-        this.subview('.progress-spin').off();
-    },
-
-    afterInitialize: function () {
-        pp.View.AnyCollection.prototype.afterInitialize.apply(this, arguments);
-        this.subview('.progress-spin').on(); // app.js fetches the collection for the first time immediately
-
-        this.collection.once('reset', this.noProgress, this);
-        this.listenTo(this.collection, 'error', this.noProgress);
-        this.render();
-    },
-
-    showMore: function () {
-        var that = this;
-
-        this.$('.show-more').addClass('disabled');
-        this.subview('.progress-spin').on();
-
-        this.collection.fetchMore(100, {
-            error: function (collection, response) {
-                pp.app.onError(undefined, response);
-            }
-        }).always(function () {
-            that.noProgress();
-        });
-    },
-
     generateItem: function (model) {
         return new pp.views.UserSmall({
             model: model
         });
     },
-
-    afterRender: function () {
-        pp.View.AnyCollection.prototype.afterRender.apply(this, arguments);
-        this.$el.find('[data-toggle=tooltip]').tooltip('show');
-    }
 });
