a2a7666b707aad6ffe12fb728df1b9a9f746ed18
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-02-21 00:38:50 +0000

    third (and default) /explore tab: "latest"

--- www/models/quest-collection.js
+++ www/models/quest-collection.js
@@ -1,6 +1,6 @@
 pp.models.QuestCollection = pp.Collection.WithCgiAndPager.extend({
     defaultCgi: ['comment_count=1'],
     baseUrl: '/api/quest',
-    cgi: ['user', 'status', 'limit', 'offset', 'sort'],
+    cgi: ['user', 'status', 'limit', 'offset', 'sort', 'order'],
     model: pp.models.Quest
 });
--- www/views/explore.js
+++ www/views/explore.js
@@ -7,32 +7,41 @@ pp.views.Explore = pp.View.Common.extend({
     },
 
     subviews: {
-        '#open-quests-tab': function () { return this.questSubview('open') },
-        '#closed-quests-tab': function () { return this.questSubview('closed') }
+        '#latest-quests-tab': function () {
+            return this.questSubview({ order: 'desc' });
+        },
+        '#open-quests-tab': function () {
+            return this.questSubview({ status: 'open', sort: 'leaderboard' });
+        },
+        '#closed-quests-tab': function () {
+            return this.questSubview({ status: 'closed', sort: 'leaderboard' });
+        }
     },
 
     afterInitialize: function () {
         _.bindAll(this);
     },
 
-    questSubview: function (st) {
-        var collection = new pp.models.QuestCollection([], {
-           'status': st,
-           'sort': 'leaderboard',
-           'limit': 100
-        });
+    questSubview: function (options) {
+
+        options.limit = 100;
+        var collection = new pp.models.QuestCollection([], options);
 
         // duplicates server-side sorting logic!
-        collection.comparator = function(m1, m2) {
-            if (m1.like_count() > m2.like_count()) return -1; // before
-            if (m2.like_count() > m1.like_count()) return 1; // after
+        if (options.sort && options.sort == 'leaderboard') {
+            collection.comparator = function(m1, m2) {
+                if (m1.like_count() > m2.like_count()) return -1; // before
+                if (m2.like_count() > m1.like_count()) return 1; // after
 
-            if (m1.comment_count() > m2.comment_count()) return -1;
-            if (m2.comment_count() > m1.comment_count()) return 1;
-            return 0; // equal
-        };
+                if (m1.comment_count() > m2.comment_count()) return -1;
+                if (m2.comment_count() > m1.comment_count()) return 1;
+                return 0; // equal
+            };
+        }
         collection.fetch();
-        collection.sort();
+        if (options.sort) {
+            collection.sort();
+        }
 
         return new pp.views.QuestCollection({
             collection: collection,
