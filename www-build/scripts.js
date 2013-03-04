57f77b9226f78691e501bbdcfedd7d54cab9dddf
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-03-04 23:39:31 +0000

    rebuild static

--- www-build/scripts.js
+++ www-build/scripts.js
@@ -977,11 +977,11 @@ pp.views.ConfirmEmail = pp.View.Common.extend({
         .done(function () {
             $('.alert').alert('close');
             pp.app.view.notify('success', 'Email confirmed.');
-            pp.app.router.navigate('/', { trigger: true });
+            pp.app.router.navigate('/', { trigger: true, replace: true });
         })
         .fail(function (response) {
             pp.app.onError(false, response);
-            pp.app.router.navigate('/', { trigger: true });
+            pp.app.router.navigate('/', { trigger: true, replace: true });
         });
     }
 });
