4bfb5e5edd5d66671eba4ec06148f7d03217c6bf
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-03-09 12:25:49 +0000

    shoutbox.pl script

---
+++ app/shoutbox.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use lib '/play/app/lib';
+
+use Play::DB qw(db);
+use Params::Validate qw(:all);
+
+sub main {
+    my ($message) = validate_pos(@_, { type => SCALAR });
+
+    my $users = db->users->list;
+
+    for my $user (@$users) {
+        db->notifications->add($user->{login}, 'shout', $message);
+    }
+}
+
+main(@ARGV) unless caller;
