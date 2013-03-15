1879e811a8480176a46a013bce07f545a36895ba
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-03-16 03:37:49 +0400

    don't use requirejs in production - use almond instead

--- build_static.pl
+++ build_static.pl
@@ -12,7 +12,7 @@ system('rm -rf www-build');
 system('cp -r www www-build');
 system(q{rm -rf www-build/views www-build/models www-build/test www-build/*.js});
 
-system('cd www && node ./tools/r.js -o name=setup mainConfigFile=setup.js out=built.js baseUrl=.');
+system('cd www && node ./tools/r.js -o name=vendors/almond include=setup mainConfigFile=setup.js out=built.js baseUrl=. wrap=true');
 rename 'www/built.js' => 'www-build/built.js';
 
 sub slurp {
@@ -22,7 +22,7 @@ sub slurp {
 }
 
 my $index_html = slurp('www-build/index.html');
-$index_html =~ s{<script data-main="/setup"}{<script data-main="/built"} or die "Can't find the main requirejs <script> tag";
+$index_html =~ s{\Q<script data-main="/setup" src="/vendors/require.js">\E}{<script src="/built.js">} or die "Can't find the main requirejs <script> tag";
 
 open my $fh, '>', 'www-build/index.html';
 print {$fh} $index_html;
