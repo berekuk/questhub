0804c6f6436d00667020f356353d96f6ee8fcccf
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-03-22 23:08:49 +0000

    basic quest-small view tests

--- www/test/spec/comment.js
+++ www/test/spec/comment.js
@@ -2,22 +2,23 @@ define([
     'models/comment',
     'views/comment/normal'
 ], function (CommentModel, Comment) {
-    describe('comments', function () {
-        it('render', function () {
-            var model = new CommentModel({
-                "body" : "aaa",
-                "ts" : 1363395653,
-                "body_html" : "aaa\n",
-                "quest_id" : "5143c351dd3d73910c00000e",
-                "author" : "ooo"
-            });
-            var view = new Comment({ model: model });
+    describe('comments render', function () {
+        var model = new CommentModel({
+            "body" : "aaa",
+            "ts" : 1363395653,
+            "body_html" : "aaa\n",
+            "quest_id" : "5143c351dd3d73910c00000e",
+            "author" : "ooo"
+        });
+        var view = new Comment({ model: model });
 
-            view.render();
+        view.render();
+        it('comment body', function () {
             expect(view.$el.html()).toContain('aaa');
+        });
 
-            // FIXME - fails because partials are not loaded via requirejs
-            // expect(view.$el.html()).toContain('ooo');
+        it('comment author', function () {
+            expect(view.$el.html()).toContain('ooo');
         });
     });
 });
