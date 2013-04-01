261355b7e5e4bb44dc96694b116f47c5ff2fd138
Pattarawat Chormai <goonnow@rocketmail.com>
2013-04-01 23:59:46 +0700

    Change limit to 101 ( Why ? )

--- www/models/event-collection.js
+++ www/models/event-collection.js
@@ -7,10 +7,9 @@ define([
         cgi: ['limit', 'offset', 'types'],
         model: Event,
         setTypes: function(types) {
-            console.log(types);
             this.options.types = types;
             this.options.offset = 0;
-            this.options.limit = 100;
+            this.options.limit = 101;
             this.fetch(this.options);
         }
     });
