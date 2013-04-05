844d80b31f73eb77ef6afe7edb5472524b8510c5
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-04-05 20:59:14 +0000

    quest-big: test edit button behavior

--- www/test/spec/quest-big.js
+++ www/test/spec/quest-big.js
@@ -5,6 +5,10 @@ define([
 ], function (QuestBig, QuestModel) {
     describe('quest-big', function () {
 
+        beforeEach(function () {
+            spyOn($, 'ajax');
+        });
+
         describe('render', function () {
             describe('non-empty team', function () {
                 var model = new QuestModel({
@@ -55,6 +59,52 @@ define([
                 });
             });
 
+        });
+
+        describe('edit', function () {
+
+            var createView = function () {
+                model = new QuestModel({
+                    "ts" : 1360197975,
+                    "status" : "closed",
+                    "_id" : "5112f9577a8f1d370b000002",
+                    "team" : ["badger"],
+                    "name" : "Badger Badger",
+                    "author" : "jonti",
+                    "tags" : ["feature"],
+                    "likes": ["mushroom", "snake"]
+                });
+
+                view = new QuestBig({ model: model });
+                view.render();
+                return view;
+                console.log(view.$el.find('h2').html());
+            };
+
+            describe('before edit is clicked', function () {
+                var view = createView();
+
+                it('title is visible', function () {
+                    expect(view.$el.find('h2 .quest-title')).not.toHaveCss({ display: 'none' });
+                });
+
+                it('input is hidden', function () {
+                    expect(view.$el.find('h2 input')).toHaveCss({ display: 'none' });
+                });
+            });
+
+            describe('after edit is clicked', function () {
+                var view = createView();
+                view.$('.edit').click();
+
+                it('title is hidden', function () {
+                    expect(view.$el.find('h2 .quest-title')).toHaveCss({ display: 'none' });
+                });
+
+                it('input is visible', function () {
+                    expect(view.$el.find('h2 input')).not.toHaveCss({ display: 'none' });
+                });
+            });
         });
 
     });
