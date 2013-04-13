6339bcb8c5c02e509a18a1af3406ea8e956250a3
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-04-13 20:52:38 +0400

    create /data/storage/email

--- cookbooks/play-perl/recipes/backend.rb
+++ cookbooks/play-perl/recipes/backend.rb
@@ -2,7 +2,8 @@ cpan_module 'Flux::File'
 cpan_module 'MMCLERIC/Flux-Format-JSON-1.00.tar.gz' # until reindexing is over
 
 directory "/data/pumper"
+directory "/data/storage/email"
+
 cron "email-pumper" do
     command "/play/backend/pumper/sendmail.pl >>/data/pumper/sendmail.log 2>>/data/pumper/sendmail.err.log"
 end
-
