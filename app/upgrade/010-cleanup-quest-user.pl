527001311f13bc3d5cf5847b6341201799f5310d
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-04-24 00:38:13 +0000

    upgrade 010 - clean quest.user

---
+++ app/upgrade/010-cleanup-quest-user.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/env perl
+
+use strictures 1;
+use strict;
+use warnings;
+
+use 5.010;
+
+use lib '/play/backend/lib';
+use Play::Mongo;
+
+my $qc = Play::Mongo->db->get_collection('quests');
+$qc->update(
+    { },
+    { '$unset' => { user => 1 } },
+    { safe => 1, multiple => 1 }
+);
