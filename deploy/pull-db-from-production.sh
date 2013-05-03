ec45118a2dc1cfd7b46de0dbdde71421e94f43cf
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-05-03 23:44:39 +0400

    pull-db-from-production runs mongorestore

--- deploy/pull-db-from-production.sh
+++ deploy/pull-db-from-production.sh
@@ -11,6 +11,7 @@ scp -r ubuntu@play-perl.org:dump/play ./dump/play-perl
 ssh ubuntu@questhub.io "sh -c 'rm -rf dump && mongodump -d play'"
 scp -r ubuntu@questhub.io:dump/play ./dump/play-qh
 
+vagrant ssh -c 'cd /play && mongorestore'
 vagrant ssh -c 'cd /play && ./deploy/merge-realms.pl'
 
 ## to avoid accidentally sending emails to users while debugging
