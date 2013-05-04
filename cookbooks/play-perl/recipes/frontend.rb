2d86dc1dfb013759a450a358c1b3eda8e58c2474
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-05-05 03:51:18 +0400

    configure seoserver in cookbooks

--- cookbooks/play-perl/recipes/frontend.rb
+++ cookbooks/play-perl/recipes/frontend.rb
@@ -27,4 +27,27 @@ template "/etc/nginx/sites-enabled/old" do
   notifies :restart, "service[nginx]"
 end
 
+include_recipe "ubic"
+
+ppa "chris-lea/node.js"
+package "nodejs"
+ppa "antono/phantomjs"
+package "phantomjs"
+
+execute "install-seoserver" do
+  command "npm install -g seoserver"
+end
+directory '/data/seoserver' # logs
+ubic_service "seoserver" do
+  action [:install, :start]
+end
+template "/etc/logrotate.d/seoserver" do
+  source "logrotate.conf.erb"
+  mode 0644
+  variables({
+    :log => '/data/seoserver/seoserver.log /data/seoserver/seoserver.err.log',
+    :postrotate => 'ubic reload seoserver',
+  })
+end
+
 service "nginx"
