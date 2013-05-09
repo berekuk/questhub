21339c6cd5d813427314eab071a7281ca564ab63
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-05-09 14:58:47 +0000

    backup-production.pl: hardcode questhub.io; mini synopsis

--- deploy/backup-production.pl
+++ deploy/backup-production.pl
@@ -3,6 +3,12 @@
 use strict;
 use warnings;
 
+=head1 SYNOPSIS
+
+  backup-production.pl
+
+=cut
+
 use Getopt::Long 2.33;
 use Pod::Usage;
 
@@ -12,8 +18,8 @@ use autodie qw(:all);
 sub main {
     GetOptions() or pod2usage(2);
 
-    pod2usage(2) unless @ARGV == 1;
-    my $name = shift @ARGV;
+    pod2usage(2) unless @ARGV == 0;
+    my $name = 'questhub.io';
 
     system(qq{ssh ubuntu\@$name "sh -c 'rm -rf dump && rm -f backup.tar.gz && mongodump -d play && tar cfvz backup.tar.gz dump'"});
     system(qq{scp ubuntu\@$name:backup.tar.gz .});
