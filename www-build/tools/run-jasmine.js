9e22abea45d23e66052aba8068e3d8c817d314a0
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-05-09 23:47:01 +0400

    rebuild static

--- www-build/tools/run-jasmine.js
+++ www-build/tools/run-jasmine.js
@@ -52,35 +52,44 @@ page.open(system.args[1], function(status){
     if (status !== "success") {
         console.log("Unable to access network");
         phantom.exit();
-    } else {
-        waitFor(function(){
-            return page.evaluate(function(){
-                return document.body.querySelector('.symbolSummary .pending') === null
-            });
-        }, function(){
-            var exitCode = page.evaluate(function(){
-                console.log('');
-                console.log(document.body.querySelector('.description').innerText);
-                var list = document.body.querySelectorAll('.results > #details > .specDetail.failed');
-                if (list && list.length > 0) {
-                  console.log('');
-                  console.log(list.length + ' test(s) FAILED:');
-                  for (i = 0; i < list.length; ++i) {
-                      var el = list[i],
-                          desc = el.querySelector('.description'),
-                          msg = el.querySelector('.resultMessage.fail');
-                      console.log('');
-                      console.log(desc.innerText);
-                      console.log(msg.innerText);
-                      console.log('');
-                  }
-                  return 1;
-                } else {
-                  console.log(document.body.querySelector('.alert > .passingAlert.bar').innerText);
-                  return 0;
-                }
-            });
-            phantom.exit(exitCode);
-        });
+        return;
     }
+
+    waitFor(function(){
+        return page.evaluate(function(){
+            return (
+                document.body.querySelector('.symbolSummary .pending') === null
+                && document.body.querySelector('.description') != null
+            );
+        });
+    }, function(){
+        var exitCode = page.evaluate(function(){
+            console.log('');
+            var list = document.body.querySelectorAll('.results > #details > .specDetail.failed');
+            if (list && list.length > 0) {
+              console.log(
+                  '\033[00;31m'
+                  + list.length + ' test(s) FAILED:'
+                  + '\033[00m'
+              );
+              for (i = 0; i < list.length; ++i) {
+                  var el = list[i],
+                      desc = el.querySelector('.description'),
+                      msg = el.querySelector('.resultMessage.fail');
+                  console.log('');
+                  console.log(desc.innerText);
+                  console.log(msg.innerText);
+              }
+              return 1;
+            } else {
+              console.log(
+                  '\033[00;32m'
+                  + document.body.querySelector('.alert > .passingAlert.bar').innerText
+                  + '\033[00m'
+              );
+              return 0;
+            }
+        });
+        phantom.exit(exitCode);
+    });
 });
