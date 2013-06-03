ae011465347eb77ace2e8664a246e1a6039c746d
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-06-03 23:18:15 +0000

    fix shoutbox - realm param, Moo::Runnable

--- app/shoutbox.pl
+++ app/shoutbox.pl
@@ -1,23 +1,31 @@
 #!/usr/bin/env perl
 
-use 5.012;
-use warnings;
-
 use lib '/play/backend/lib';
+use Moo;
+with 'Moo::Runnable';
+use MooX::Options;
 
 use Play::DB qw(db);
 
 use Type::Params qw(validate);
 use Types::Standard qw(Str);
 
-sub main {
-    my ($message) = validate(\@_, Str);
+option 'realm' => (
+    is => 'ro',
+    format => 's',
+    required => 1,
+);
 
-    my $users = db->users->list;
+sub run {
+    my $self = shift;
+    my $message = join '', <STDIN>;
+
+    warn "realm: ".$self->realm;
+    my $users = db->users->list({ realm => $self->realm });
 
     for my $user (@$users) {
         db->notifications->add($user->{login}, 'shout', $message);
     }
 }
 
-main(join '', <>) unless caller;
+__PACKAGE__->run_script;
