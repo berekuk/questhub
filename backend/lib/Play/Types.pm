ec1ca07ea9ff84e7a02d9f5a44f9e82b61d281c6
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-06-18 18:41:00 +0000

    db->comments uses compiled type-tiny checks

--- backend/lib/Play/Types.pm
+++ backend/lib/Play/Types.pm
@@ -5,7 +5,11 @@ use warnings;
 
 use Type::Library
     -base,
-    -declare => qw( Id Login ImageSize ImageUpic );
+    -declare => qw(
+        Id Login
+        ImageSize ImageUpic
+        CommentParams
+    );
 use Type::Utils;
 use Types::Standard qw( Str StrMatch Dict );
 
@@ -16,5 +20,12 @@ declare ImageSize, as enum [qw/ small normal /];
 
 declare ImageUpic,
     as Dict[small => Str, normal => Str];
+
+declare CommentParams,
+    as Dict[
+        quest_id => Id,
+        body => Str,
+        author => Login,
+    ];
 
 1;
