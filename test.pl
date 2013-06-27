919ac54923f513b9099b4c933b9cc18b5d12fa6d
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-06-27 20:44:30 +0000

    testing flags

--- test.pl
+++ test.pl
@@ -6,6 +6,12 @@ use warnings;
 use IPC::System::Simple;
 use autodie qw(:all);
 
-system('cd backend && prove');
-system('cd app && prove');
-system('phantomjs ./www/tools/run-jasmine.js http://localhost:81/test/index.html');
+sub main {
+    my $action = shift;
+    $action ||= 'abf';
+    system('cd backend && prove') if $action =~ /b/;
+    system('cd app && prove') if $action =~ /a/;
+    system('phantomjs ./www/tools/run-jasmine.js http://localhost:81/test/index.html') if $action =~ /f/;
+}
+
+main(@ARGV) unless caller;
