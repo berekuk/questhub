121276f981590affd9ce3f5cf2377d6428051ec6
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-07-01 17:03:08 +0400

    coffee-test ubic service

--- cookbooks/play-perl/recipes/dev.rb
+++ cookbooks/play-perl/recipes/dev.rb
@@ -37,7 +37,7 @@ cpan_module "IPC::System::Simple"
 
 gem_package 'sass'
 gem_package 'rb-inotify' do
-  version '0.8.8'
+  version '0.9'
 end
 ubic_service "sass" do
   action [:install, :start]
@@ -46,5 +46,8 @@ end
 include_recipe "npm"
 npm_package "coffee-script"
 ubic_service "coffee" do
+  action [:install, :start]
+end
+ubic_service "coffee-test" do
   action [:install, :start]
 end
