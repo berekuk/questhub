24dc4e4c7c937955b3f98ac90b540724bdee1ac4
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-07-17 13:10:44 +0000

    reset realms on clear_mongo.sh

--- app/clear_mongo.sh
+++ app/clear_mongo.sh
@@ -15,4 +15,4 @@ END
 (echo 'use play'; echo "$CODE") | mongo
 (echo 'use play_test'; echo "$CODE") | mongo
 
-perl -I/play/backend/lib -e 'use Play::DB qw(db); db->ensure_indices'
+perl -I/play/backend/lib -e 'use Play::DB qw(db); db->ensure_indices; db->realms->reset_to_initial'
