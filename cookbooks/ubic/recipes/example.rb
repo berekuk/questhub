b25951e336e0e652b8cc4199d552c6faaa3a8f34
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2015-05-09 22:32:18 +0300

    remove cookbooks (yay)

--- cookbooks/ubic/recipes/example.rb
+++
@@ -1,6 +0,0 @@
-include_recipe "ubic"
-
-ubic_service "foo" do
-  action [:install, :start]
-  # put your template into templates/ubic-service-foo.erb
-end
--- cookbooks/ubic/resources/service.rb
+++
@@ -1,7 +0,0 @@
-actions :install, :start, :stop, :restart
-default_action :install
-
-attribute :name, :kind_of => String, :name_attribute => true
-
-# private
-attribute :service
