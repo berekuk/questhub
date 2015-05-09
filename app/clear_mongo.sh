272445b4df01d16925a87972f6a8d51c4f015903
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2015-05-10 00:26:24 +0300

    clear_mongo.sh doesn't use mongo command line client

--- app/clear_mongo.sh
+++ app/clear_mongo.sh
@@ -1,17 +1,4 @@
 #!/bin/sh
 
-CODE=$(cat <<END
-use play
-db.posts.drop()
-db.users.drop()
-db.sessions.drop()
-db.comments.drop()
-db.events.drop()
-db.realms.drop()
-END
-)
-
-(echo 'use play'; echo "$CODE") | mongo
-(echo 'use play_test'; echo "$CODE") | mongo
-
+perl -I/play/backend/lib -e 'use Play::Mongo; Play::Mongo->db->eval("db.$_.drop()") for qw/posts users sessions comments events realms/'
 perl -I/play/backend/lib -e 'use Play::DB qw(db); db->ensure_indices; db->realms->reset_to_initial'
