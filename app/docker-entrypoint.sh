e2f427d3cc8eb8c57184b041ce90fa30ee9deaf9
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2015-05-11 00:42:49 +0300

    app: plackup -r instead of auto_reload

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -5,4 +5,13 @@ cd $APP_HOME
 erb ./config.yml.erb >./config.yml
 LOG_DIR=/data/dancer
 mkdir -p $LOG_DIR
-plackup --server Starman --port 80 bin/app.pl >>$LOG_DIR/stdout.log 2>>$LOG_DIR/stderr.log
+
+PLACKUP_FLAGS=''
+if [ -n $QH_DEV ]; then
+    PLACKUP_FLAGS=-r
+    export DANCER_ENVIRONMENT=production
+else
+    export DANCER_ENVIRONMENT=development
+fi
+
+plackup --server Starman --port 80 $PLACKUP_FLAGS bin/app.pl >>$LOG_DIR/stdout.log 2>>$LOG_DIR/stderr.log
