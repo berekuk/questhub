99da3d9123ad30009dd1deff43b83f9d3ffc793c
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2015-05-16 14:20:34 +0300

    app: fix dancer environments; fix reload on development

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -7,11 +7,8 @@ LOG_DIR=/data/dancer
 mkdir -p $LOG_DIR
 
 PLACKUP_FLAGS=''
-if [ $QH_DEV = 0 ]; then
+if [ $QH_DEV = 1 ]; then
     PLACKUP_FLAGS=-r
-    export DANCER_ENVIRONMENT=production
-else
-    export DANCER_ENVIRONMENT=development
 fi
 
-plackup --server Starman --port 80 $PLACKUP_FLAGS bin/app.pl >>$LOG_DIR/stdout.log 2>>$LOG_DIR/stderr.log
+plackup --server Starman --port 80 $PLACKUP_FLAGS app.pl >>$LOG_DIR/stdout.log 2>>$LOG_DIR/stderr.log
